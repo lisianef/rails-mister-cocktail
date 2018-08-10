d932195981a8792bcb9be7058ba6b0b68fbf608d
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-10 11:59:19 +0200

    Setup Clever Cloud config files

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,37 +11,59 @@ Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-# uri = URI.parse(url)
-# response = uri.open.read
-# r = JSON.parse(response)
+uri = URI.parse(url)
+response = uri.open.read
+r = JSON.parse(response)
 
-# r["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient["strIngredient1"])
-# end
-# puts "Done"
+r["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+puts "Ingredients done"
 
-cocktail_a = Cocktail.create!(name: "boubi")
-cocktail_b = Cocktail.create!(name: "truc")
+drinks = ["Coffee Sip", "Milk Blood", "Red Riddle", "Garlic Wacker",
+"Sanguine Grog", "Brilliant Light Ale", "Green Roar", "Pineapple Lagoon",
+"Grapefruit Dark Beer", "Gin Giant", "Spring Petal", "Pink Sunset",
+"Silent Thriller", "Low Velvet", "Minty Crush", "Gin Breeze", "Tiny Duke",
+"Noxious Rum", "Passion Fruit Paralyzer", "Silent Fluffy"]
 
-dose1 = Dose.create!({
-  cocktail: cocktail_a,
-  ingredient: orange,
-  description: "6cl"
-})
+drinks.each do |drink|
+  Cocktail.create!(name: "#{drink}")
+end
+puts "Cocktails done"
 
-dose2 = Dose.create!({
-  cocktail: cocktail_a,
-  ingredient: gin,
-  description: "3cl"
-})
-dose2.save
+20.times do
+  Dose.create!(
+    description: "#{rand(1..10)} cl",
+    cocktail:  Cocktail.order("RANDOM()").first,
+    ingredient: Ingredient.order("RANDOM()").first
+  )
+end
+puts "Doses OK"
 
-dose3 = Dose.create!({
-  cocktail: cocktail_b,
-  ingredient: lemon,
-  description: "1cl"
-})
-dose3.save
+  # Dose.create!({
+  #   cocktail: drink,
+  #   ingredient: Ingredient.sample(0..5),
+  #   description: rand(1..10)
+  # })
+# dose1 = Dose.create!({
+#   cocktail: cocktail_a,
+#   ingredient: orange,
+#   description: "6cl"
+# })
+
+# dose2 = Dose.create!({
+#   cocktail: cocktail_a,
+#   ingredient: gin,
+#   description: "3cl"
+# })
+# dose2.save
+
+# dose3 = Dose.create!({
+#   cocktail: cocktail_b,
+#   ingredient: lemon,
+#   description: "1cl"
+# })
+# dose3.save
 
